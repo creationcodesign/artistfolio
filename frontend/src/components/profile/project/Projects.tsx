92cc918c179846f69e738e1af3d61b8fedb6bb14
on <natalialexandra015@gmail.com>
2024-10-28 16:36:36 +0100

    add projects

---
+++ frontend/src/components/profile/project/Projects.tsx
@@ -0,0 +1,16 @@
+import Project from "./Project";
+
+
+export default function Projects() {
+    return (
+        <div className="projects">
+            <h2>Projects</h2>
+
+            <Project />
+
+            <button>
+                Add new one
+            </button>
+        </div>
+    )
+}
