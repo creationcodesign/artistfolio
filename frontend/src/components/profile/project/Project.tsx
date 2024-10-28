880bfc38ec5f2840a5f2bf554d007d485f69526e
on <natalialexandra015@gmail.com>
2024-10-28 16:36:45 +0100

    add project

---
+++ frontend/src/components/profile/project/Project.tsx
@@ -0,0 +1,12 @@
+export default function Project() {
+    return (
+        <div className="project">
+            <h3>Project Name</h3>
+            <img src="" alt="project image" />
+
+            <div className="project-actions">
+
+            </div>
+        </div>
+    )
+}
