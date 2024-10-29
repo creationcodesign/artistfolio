b4116e65780710dfdc4e3485c4c7c8cc1db19474
on <natalialexandra015@gmail.com>
2024-10-29 18:58:13 +0100

    add profile route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,12 +1,14 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Profile from '../pages/profile/Profile'
 
 export default function AppRoutes() {
     return (
         <Routes>
             <Route path='/' element={<Home />} />
             <Route path='/about' element={<About />} />
+            <Route path='/profile' element={<Profile />} />
         </Routes>
     )
 }
