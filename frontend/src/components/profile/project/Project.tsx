000f33a1fd65fe1426342faa82152f66ea80fa39
on <natalialexandra015@gmail.com>
2024-10-31 16:07:49 +0100

    project setup

--- frontend/src/components/profile/project/Project.tsx
+++ frontend/src/components/profile/project/Project.tsx
@@ -1,11 +1,21 @@
-export default function Project() {
+import { Icon } from '@ailibs/feather-react-ts'
+import defaultImg from '../../../assets/images/default-img-small.png'
+
+
+export default function Project({ setProjectAction }: any) {
     return (
         <div className="project">
-            <h3>Project Name</h3>
-            <img src="" alt="project image" />
+            <div className='project-content'>
+                <img src={defaultImg} alt="project image" />
+                <h3>Project Name</h3>
+            </div>
 
             <div className="project-actions">
-
+                <Icon name="eye" onClick={() => setProjectAction('view')} />
+                <div className='project-actions__icons'>
+                    <Icon name="trash-2" onClick={() => setProjectAction('delete')} />
+                    <Icon name="edit-3" onClick={() => setProjectAction('edit')} />
+                </div>
             </div>
         </div>
     )
