fcbbd101f084258e7dc990dfb99a7e7368636ef2
on <natalialexandra015@gmail.com>
2024-10-31 16:09:34 +0100

    profile page

--- frontend/src/pages/profile/Profile.tsx
+++ frontend/src/pages/profile/Profile.tsx
@@ -1,7 +1,29 @@
+import { useState } from "react";
+import Sidebar from "../../components/profile/Sidebar";
+import About from "../About";
+import Projects from "../../components/profile/project/Projects";
+
 export default function Profile() {
+    // const actions = ['dashboard', 'projects', 'about', 'settings', 'account'];
+    const [selectedAction, setSelectedAction] = useState('dashboard');
+
     return (
         <div className='profile-page'>
             <h1>Profile</h1>
+
+            <Sidebar
+                selectedAction={selectedAction}
+                setSelectedAction={setSelectedAction}
+            />
+
+            {selectedAction === 'dashboard' && <About />
+                ||
+                selectedAction === 'projects' && <Projects />
+                // ||
+                // selectedAction === 'settings' && <Settings />
+                // ||
+                // selectedAction === 'account' && <Account />
+            }
         </div>
     )
 }
