f8adf457e62b98791b753516a140a00afd17bd2c
on <natalialexandra015@gmail.com>
2024-10-31 16:08:00 +0100

    projects

--- frontend/src/components/profile/project/Projects.tsx
+++ frontend/src/components/profile/project/Projects.tsx
@@ -1,16 +1,32 @@
+import { useState } from "react";
+import { Icon } from "@ailibs/feather-react-ts";
 import Project from "./Project";
+import CreateProject from "./CreateProject";
 
 
 export default function Projects() {
+    const [projectAction, setProjectAction] = useState<string>('create')
+
     return (
-        <div className="projects">
-            <h2>Projects</h2>
+        <div className="projects-section">
+            <div className="projects-header">
+                <h2>Projects</h2>
+                <button onClick={() => setProjectAction('create')} className="btn-action">
+                    <Icon name="plus-circle" />
+                    add new project
+                </button>
+            </div>
 
-            <Project />
+            <div className="projects">
+                <Project setProjectAction={setProjectAction} />
+                <Project setProjectAction={setProjectAction} />
+            </div>
 
-            <button>
-                Add new one
-            </button>
+
+            {projectAction === 'create' &&
+                <CreateProject />
+            }
+
         </div>
     )
 }
