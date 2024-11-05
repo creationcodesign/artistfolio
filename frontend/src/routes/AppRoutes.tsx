f029383bcb6a9c73afb544523d62120aeb95c70a
on <natalialexandra015@gmail.com>
2024-11-05 14:19:29 +0100

    protected route

--- frontend/src/routes/AppRoutes.tsx
+++ frontend/src/routes/AppRoutes.tsx
@@ -1,14 +1,21 @@
 import { Route, Routes } from 'react-router-dom'
 import Home from '../pages/Home'
 import About from '../pages/About'
+import Auth from '../pages/Auth'
 import Profile from '../pages/profile/Profile'
+import ProtectedRoute from './ProtectedRoute'
 
 export default function AppRoutes() {
+
     return (
         <Routes>
             <Route path='/' element={<Home />} />
-            <Route path='/about' element={<About />} />
-            <Route path='/profile' element={<Profile />} />
+            <Route path='/auth' element={<Auth />} />
+            <Route element={<ProtectedRoute />}>
+                <Route path='/auth/profile' element={<Profile />} />
+                <Route path='/auth/about' element={<About />} />
+            </Route>
+            <Route path="*" element={<Home />} />
         </Routes>
     )
 }
