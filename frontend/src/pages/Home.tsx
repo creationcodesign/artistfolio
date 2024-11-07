da084e8550a80c6770acddb783111286e1f0b0ad
on <natalialexandra015@gmail.com>
2024-11-07 11:33:50 +0100

    add sections

--- frontend/src/pages/Home.tsx
+++ frontend/src/pages/Home.tsx
@@ -1,9 +1,20 @@
-
+import AboutMe from "../components/portfolio/AboutMe";
+import PortfolioLayout from "../layout/PortfolioLayout";
+import Hero from "../components/portfolio/Hero";
+import SelectedWork from "../components/portfolio/SelectedWork";
+import Skills from "../components/portfolio/Skills";
+import Contact from "../components/portfolio/Contact";
 
 export default function Home() {
     return (
-        <main className='home-page'>
-            <h1>Home</h1>
-        </main>
+        <PortfolioLayout>
+            <div className='home-page'>
+                <Hero />
+                <SelectedWork />
+                <AboutMe />
+                <Skills />
+                <Contact />
+            </div>
+        </PortfolioLayout>
     )
 }
