9f031d9b41e24e6585aff822b2adb28b2ab14000
on <natalialexandra015@gmail.com>
2024-11-07 11:33:15 +0100

    contact section

---
+++ frontend/src/components/portfolio/Contact.tsx
@@ -0,0 +1,15 @@
+export default function Contact() {
+    return (
+        <div className="contact-section section" id="contact">
+            <h2 className="section-title">Get in Touch</h2>
+            <p>
+                I'm always excited to connect with fellow creatives and audiences who  share a passion for audio-visual art.
+            </p>
+            <p>Whether you're interested in a collaboration, commissioning a project, or simply want to discuss the latest trends in audio-visual media, feel free to reach out.</p>
+            <p>Let's create something extraordinary together.</p>
+            <button className="btn-primary">
+                Contact Me
+            </button>
+        </div>
+    )
+}
