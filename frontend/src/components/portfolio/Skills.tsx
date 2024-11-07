23ffae63c405b5ec7cdb0627c69ee54eeb6c6deb
on <natalialexandra015@gmail.com>
2024-11-07 11:32:58 +0100

    skills section

---
+++ frontend/src/components/portfolio/Skills.tsx
@@ -0,0 +1,20 @@
+export default function Skills() {
+    return (
+        <div className="skills-section section" id="skills">
+            <div className="skills-content">
+                <h2 className="section-title">Skills and Tools</h2>
+                <p>I utilize a diverse array of tools and techniques to bring my visions to life. </p>
+                <p>On the audio side, I specialize in sound design, music composition, audio engineering, using  software like Ableton Live, Logic Pro and Pro Tools. </p>
+                <p>For the visual component, I work with video editing, animation, motion graphics, leveraging  tools such as Adobe Premiere Pro, After  Effects, Final Cut Pro and Davince Resolve.</p>
+            </div>
+            <div className="tools">
+                <span className="tag">Adobe After Effects</span>
+                <span className="tag">Adobe Premiere Pro</span>
+                <span className="tag">Adobe Photoshop</span>
+                <span className="tag">Adobe Illustrator</span>
+                <span className="tag">Final Cut Pro</span>
+                <span className="tag">AI Tools</span>
+            </div>
+        </div>
+    )
+}
