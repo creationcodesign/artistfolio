e332edf50df15c90b5ad78f8e1e44b28788db4b2
on <natalialexandra015@gmail.com>
2024-11-09 11:16:35 +0100

    hook to fech about section

---
+++ frontend/src/hooks/useAboutSection.tsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react"
+import axios from 'axios';
+import { IAbout } from "../interface/IAbout";
+
+export default function useAboutSection() {
+    const [aboutData, setAboutData] = useState<IAbout | null>()
+    const [loading, setLoading] = useState<boolean>(false)
+
+    useEffect(() => {
+        axios.get('/api/about')
+            .then(response => {
+                console.log("response:", response.data);
+                setAboutData(response.data);
+                setLoading(false)
+            })
+            .catch(error => {
+                setLoading(true)
+                console.error('There was an error fetching the About Me content!', error);
+            });
+    }, []);
+
+    return { aboutData, loading }
+}
