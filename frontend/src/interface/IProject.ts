4fee83ff15899d8ce462352da948a066e0a47ea3
on <natalialexandra015@gmail.com>
2024-11-17 20:48:00 +0100

    add interface projects

--- frontend/src/interface/IProject.ts
+++ frontend/src/interface/IProject.ts
@@ -5,14 +5,33 @@ export interface IProject {
     technologies: string[];
     link: string;
     thumbnail: string;
+    position: number;
+}
+
+export interface IProjectCreate {
+    name: string;
+    description: string;
+    technologies: string[];
+    link: string;
+    thumbnail: string;
+}
+
+export interface IProjectUpdate {
+    _id: string | null;
+    name: string;
+    description: string;
+    technologies: string[];
+    link: string;
+    thumbnail: string;
 }
 
 export interface ProjectStore {
     projects: IProject[];
     setProjects: (projects: IProject[]) => void;
-    createProject: (newProject: IProject) => Promise<{ success: boolean; message?: string }>;
+    createProject: (newProject: IProjectCreate) => Promise<{ success: boolean; message?: string }>;
     getProject: (id: string) => Promise<{ success: boolean; message?: string; data?: IProject }>;
     getProjects: () => Promise<{ success: boolean; message?: string; data?: IProject[] }>;
     updateProject: (id: string, updatedProject: IProject) => Promise<{ success: boolean; message?: string; data?: IProject }>;
+    updateProjectsOrder: (projects: IProject[]) => Promise<{ success: boolean; message?: string }>;
     deleteProject: (id: string) => Promise<{ success: boolean; message?: string }>;
 }
