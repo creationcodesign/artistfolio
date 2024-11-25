90094afd66df2c3c409999d3ea7926349443190c
on <natalialexandra015@gmail.com>
2024-11-25 18:39:35 +0100

    display user data on dashboard

--- frontend/src/components/profile/Dashboard.tsx
+++ frontend/src/components/profile/Dashboard.tsx
@@ -1,9 +1,29 @@
-
-
 export default function Dashboard() {
+    const storedUser = localStorage.getItem('user');
+
+    if (!storedUser) {
+        return null;
+    }
+
+    const user = JSON.parse(storedUser);
+
     return (
-        <div className="dashboard-page">
-            <h1>Dashboard</h1>
+        <div className="dashboard">
+            <h2>Welcome back</h2>
+            <div className="last-login">
+                <p>
+                    <span>Your last login was on </span>
+                    {user.lastLogin ? <span className="bold">
+                        {new Date(user.lastLogin).toLocaleString()}
+                    </span>
+                        : <span>No login history</span>
+                    }
+                </p>
+
+                <p>Browser: {user.browser.split(' ')[0]}</p>
+                <p>Platform: {user.platform.replace(/^"|"$/g, '')}</p>
+            </div>
+            <p>{user.email}</p>
         </div>
     )
 }
