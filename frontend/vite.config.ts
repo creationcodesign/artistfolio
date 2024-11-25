575042dd045750e96320d091363c135449543823
on <natalialexandra015@gmail.com>
2024-11-25 21:55:59 +0100

    change target

--- frontend/vite.config.ts
+++ frontend/vite.config.ts
@@ -7,7 +7,8 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:5000',
+        target: 'https://artistfolio.onrender.com',
+        // target: 'http://localhost:5000',
         // changeOrigin: true,
         // secure: false,
       },
