022e2e0d434321e9124ed6e651378e0b7a55337e
on <natalialexandra015@gmail.com>
2025-03-25 09:40:51 +0100

    little changes

--- backend/server.js
+++ backend/server.js
@@ -35,10 +35,12 @@ app.use('/api/about', aboutSectionRouter);
 app.use('/api/skills', skillsSectionRouter);
 app.use('/api/contact', contactSectionRouter);
 
-if (process.env.NODE_ENV === 'production') {
-    app.use(express.static(path.join(__dirname, '/frontend/dist')));
-    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'dist', 'index.html')));
-}
+// Serve static files
+app.use(express.static(path.join(__dirname, './frontend/dist')));
+// Handle any requests that don't match our routes
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, './frontend/dist', 'index.html'));
+});
 
 // Connect to the database and start the server
 const startServer = async () => {
